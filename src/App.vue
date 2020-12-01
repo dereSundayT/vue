9daf00e59b6b372dc69e63ae0bcd65b491ca5689
Sunday Dere Toluwani <toluwanisundaydere@gmail.com>
2020-12-01 21:35:10 +0100

    crd operation completed for the todo app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <Header/>
+  <AddTodo v-on:add-todo="addTodo"/>
+<Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos.vue';
+import AddTodo from './components/AddTodo.vue';
+import Header from './components/layout/Header.vue';
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Todos,
+    Header,
+    AddTodo
+
+  },
+  data(){
+    return{
+      msg:'Hello',
+      todos:[
+        {
+          id:1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id:2,
+          title: "Todo Two",
+          completed: true
+        },
+                {
+          id:3,
+          title: "Todo Three",
+          completed: false
+        },
+      ]
+    }
+  },
+  methods:{
+    deleteTodo(id){
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    },
+    addTodo(newTodo){
+      this.todos = [...this.todos,newTodo];
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  box-sizing:border-box;
+  margin: 0;
+  padding: 0;
 }
+body{
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+.btn{
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+btn:hover{
+  background:#666;
+}
+
 </style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+      <form @submit="addTodo">
+          <input type="text" v-model="title" name="title" placeholder="Add Todo....."/>
+          <input type="submit" value="Subnit" class="btn">
+
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "AddTodo",
+    data() {
+        return{
+        title: ''
+        }
+        },
+    methods:{
+        addTodo(e){
+            e.preventDefault();
+            const newTodo = {
+                id:5,
+                title:this.title,
+                completed:false
+            }
+            // send  up to parent
+            this.$emit('add-todo',newTodo);
+            this.title = '';
+        }
+    }
+}
+</script>
+
+<style scoped>
+form{
+    display:flex;
+}
+input[type="text"]{
+    flex:10;
+    padding: 5px;
+}
+input[type="submit"]{
+    flex:2
+}
+
+</style>>
+
